10488e9adeb703bc5a9ba1f3b081e89b76f83428
Marco Bungart <marco.bungart@googlemail.com>
2025-09-25 17:05:11 +0200

    Remove ConfigRoot annotation

--- src/main/java/de/turing85/quarkus/camel/jms/to/sftp/config/RouteConfig.java
+++ src/main/java/de/turing85/quarkus/camel/jms/to/sftp/config/RouteConfig.java
@@ -1,10 +1,8 @@
 package de.turing85.quarkus.camel.jms.to.sftp.config;
 
-import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
 
 @ConfigMapping(prefix = "route")
-@ConfigRoot
 public interface RouteConfig {
   String queue();
 
